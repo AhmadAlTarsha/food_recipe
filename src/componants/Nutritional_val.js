1146caa58337cdc95e04a258f1b8040355a73c95
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2023-08-13 17:37:06 +0300

    99%end Q7

--- src/componants/Nutritional_val.js
+++ src/componants/Nutritional_val.js
@@ -1,18 +1,14 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
 const Nutritional_val = ({currentRecipes}) => {
     const totalNutrient=  currentRecipes.recipe.digest
-// const arr=totalNutrient.splice(6)
-    //console.log(totalNutrient);
- const arr=totalNutrient.splice(7)
- console.log(arr);
-
+ const array=totalNutrient.splice(7)
+    console.log(currentRecipes);
 
   return (
-    <>{ arr.map((fact,i)=>{
-return <div><p>{fact.label}</p><p>{fact.total
-}</p><p>{fact.unit}</p></div>
- })}</>
+    <>{totalNutrient.map((ele)=>{
+        return <div>{ele.label} <table>{ele.unit}</table ><table>{ele.total}</table></div>
+    })}</>
   )
 }
 
--- src/componants/recipe_detiles.js
+++ src/componants/recipe_detiles.js
@@ -28,8 +28,8 @@ const Recipes_details = ({ currentRecipes, showRecipeDetail, setShowRecipeDetail
       }}>Back</button></div> <div className='main_details' ><div className='main_details_2'  ><h3>{currentRecipes.recipe.label}</h3><h4>{`caloeies : ${currentRecipes.recipe.calories} `}</h4><p>DishType : {dishType}</p><p>CuisineType : {cuisineType}</p></div> <div className='ingredient'><h3>ingredient</h3>{allIngredient}<div><button onClick={()=>{console.log(Nutritional);
 setShowNutritional(!Nutritional)
 if (!Nutritional) {
-  setShow("show")
-}else{setShow("hide")}
+  setShow("hide")
+}else{setShow("show")}
 
       }} className='Nutritional_button' >{Show} Nutritional Value  </button> </div><h3>see the Recipe in the site</h3></div>{Nutritional && <Nutritional_val currentRecipes={currentRecipes}/>}<div></div></div></div>
    
